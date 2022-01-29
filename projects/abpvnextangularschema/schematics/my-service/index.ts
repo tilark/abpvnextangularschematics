2409bfb29315d18b3b19efe83a8e7ffb66ad2508
tilark <tilark.liu@gmail.com>
2022-01-29 15:17:13 +0800

    增加了my-service的特征

---
+++ projects/abpvnextangularschema/schematics/my-service/index.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+ import { Rule } from '@angular-devkit/schematics';
+ import { generateFromFiles } from '../utility/generate-from-files';
+ import { Schema as ServiceOptions } from './schema';
+
+ export default function (options: ServiceOptions): Rule {
+   // This schematic uses an older method to implement the flat option
+   const flat = options.flat;
+   options.flat = true;
+
+   return generateFromFiles(options, {
+     'if-flat': (s: string) => (flat ? '' : s),
+   });
+ }
